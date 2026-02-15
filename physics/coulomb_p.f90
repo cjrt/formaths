b7762e948c9c268a50511619b0c210ca6fcf5593
cjt <ctalb5000@gmail.com>
2026-02-15 23:16:34 +0000

    relevant variables added

--- physics/coulomb_p.f90
+++ physics/coulomb_p.f90
@@ -5,9 +5,9 @@ module coulomb_p
    implicit none
 
 contains
-    elemental pure real(dp) function coulomb(d)
-    real(dp), intent(in) :: d
-
+    elemental pure real(dp) function coulomb(d, q1, q1)
+    real(dp), intent(in) :: d, q1, q1
+    
     real(dp), parameter :: mx_compute_epsi = epsilon(1.0_dp)
     real(dp), parameter :: softening = sqrt(mx_compute_epsi)           
             
